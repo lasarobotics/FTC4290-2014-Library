d19cd87ff725a0bfe3b04d537cbd4a9d177bcbad
Arthur Pachachura <pachachura.arthur@gmail.com>
2014-06-09 17:30:32 -0500

    Add RobotC naturalization

---
+++ robotc/lib/naturalization.h
@@ -0,0 +1,44 @@
+/**********************************************************
+
+RobotC Naturalizarition
+naturalization.h
+
+Forces all RobotC environments to use the same settings.
+Include this file at the beginning of every program.
+
+Define _DISABLE_DISPLAY to disable game controller display
+Define _FORCE_DEBUG to disable full code optimization
+
+**********************************************************/
+
+#ifndef __NATURALIZATION_
+#define __NATURALIZATION_
+#endif
+
+#ifndef _FORCE_DEBUG
+	#ifdef _DEBUG
+		#undef _DEBUG //force full code optimization
+	#endif
+#endif
+
+#if (defined(NXT) || defined(TETRIX)) && defined(_Target_Robot_) && !defined(NaturalLanguage)
+	//A OK
+#elif (defined(VEX2) || defined(NXT) || defined(TETRIX)) && defined(_Target_VirtWorld_)
+	#error "Not supported with Virtual Worlds!"
+#elif defined(NaturalLanguage)
+	#warning "You are using Natural Language.  Some errors may occur as a result."
+#elif (defined(NXT) || defined(TETRIX)) && defined(_Target_Emulator_)
+  #warning "You are using an Emulator.  Some errors may occur as a result."
+#else
+  #error "Not supported on this platform!"
+#endif
+
+#ifndef _DISABLE_DISPLAY
+	#pragma DebuggerWindows("Globals")
+	#pragma DebuggerWindows("Locals")
+	#ifdef FTC
+		#pragma DebuggerWindows("joystickGame")
+	#else
+		#pragma DebuggerWindows("joystickSimple")
+	#endif
+#endif
