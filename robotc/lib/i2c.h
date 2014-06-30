0aa2c5e98d80227659e0c27c225edc6713b25f5a
Oksisane <ehsanmasdar@gmail.com>
2014-06-29 21:16:43 -0500

    Fix formatting

--- robotc/lib/i2c.h
+++ robotc/lib/i2c.h
@@ -1,7 +1,14 @@
+/**
+* Checks a robot component for connection.
+* This function allows you to validate that a robot sensor is on and functioning.
+* @param port The expected port of the sensor.
+* @param address The expected address of the sensor.
+* @param expected The first 8 characters of the name of the expected sensor.
+*/
 bool errorcheck (int port, int address, string &expected)
 {
 	//Local vars
-  const int responsesize = 8;
+	const int responsesize = 8;
 	sbyte I2CmessageS1[4];
 	sbyte I2CreplyS1[8];
   //Setup port
@@ -27,52 +34,52 @@ bool errorcheck (int port, int address, string &expected)
 	//Request starts at byte 10, where sensor name is stored
 	I2CmessageS1[2] = 0x10;
 	switch (port){
-  			case 1:
-  			  //Send message
-  				sendI2CMsg(S1, &I2CmessageS1[0], responsesize);
-  				//Wait while message processed
-  				while (nI2CStatus[S1] == STAT_COMM_PENDING){
-							wait1Msec(2);
-				   }
-				  //Read reply
-					readI2CReply(S1, &I2CreplyS1[0], responsesize);
-  				break;
-  			case 2:
-  				sendI2CMsg(S2, &I2CmessageS1[0], responsesize);
-  				while (nI2CStatus[S2] == STAT_COMM_PENDING){
-							wait1Msec(2);
-				   }
-					readI2CReply(S2, &I2CreplyS1[0], responsesize);
-  				break;
-				case 3:
-  				sendI2CMsg(S3, &I2CmessageS1[0], responsesize);
-  				while (nI2CStatus[S3] == STAT_COMM_PENDING){
-							wait1Msec(2);
-				   }
-					readI2CReply(S3, &I2CreplyS1[0], responsesize);
-					break;
-  			case 4:
-  				sendI2CMsg(S4, &I2CmessageS1[0], responsesize);
-  				while (nI2CStatus[S4] == STAT_COMM_PENDING){
-							wait1Msec(2);
-				   }
-					readI2CReply(S4, &I2CreplyS1[0], responsesize);
-  				break;
-		}
-		//get string returned
-		string display = "";
-		int convArray[responsesize];
-		//Convert array to int array
-		for (int i=0; i<responsesize; ++i){
-			convArray[i] = I2CreplyS1[i];
-			display = display + (char)convArray[i];
-		}
-		bool debug = false;
-		//compare result to expected
-		if (display == expected){
-			 return true;
-		}
-		else {
-			return false;
-		}
+  		case 1:
+  			//Send message
+  			sendI2CMsg(S1, &I2CmessageS1[0], responsesize);
+  			//Wait while message processed
+  			while (nI2CStatus[S1] == STAT_COMM_PENDING){
+				wait1Msec(2);
+			}
+			//Read reply
+			readI2CReply(S1, &I2CreplyS1[0], responsesize);
+  			break;
+  		case 2:
+  			sendI2CMsg(S2, &I2CmessageS1[0], responsesize);
+  			while (nI2CStatus[S2] == STAT_COMM_PENDING){
+				wait1Msec(2);
+			}
+		    readI2CReply(S2, &I2CreplyS1[0], responsesize);
+  			break;
+		case 3:
+  			sendI2CMsg(S3, &I2CmessageS1[0], responsesize);
+  			while (nI2CStatus[S3] == STAT_COMM_PENDING){
+				wait1Msec(2);
+			}
+			readI2CReply(S3, &I2CreplyS1[0], responsesize);
+			break;
+  		case 4:
+  			sendI2CMsg(S4, &I2CmessageS1[0], responsesize);
+  			while (nI2CStatus[S4] == STAT_COMM_PENDING){
+				wait1Msec(2);
+			}
+			readI2CReply(S4, &I2CreplyS1[0], responsesize);
+  			break;
+	}
+	//get string returned
+	string display = "";
+	int convArray[responsesize];
+	//Convert array to int array
+	for (int i=0; i<responsesize; ++i){
+		convArray[i] = I2CreplyS1[i];
+		display = display + (char)convArray[i];
+	}
+	bool debug = false;
+	//compare result to expected
+	if (display == expected){
+		return true;
+	}
+	else {
+		return false;
+	}
 }
--- robotc/src/ErrorChecking.c
+++ robotc/src/ErrorChecking.c
@@ -1,5 +1,4 @@
 #pragma config(Sensor, S4,     HTSPB,                sensorI2CCustom9V)
-
 /***** DEFINES *****/
 //#define _FORCE_DEBUG //Uncomment to force using debug (non-optimized) mode
 #define _DISABLE_JOYDISPLAY //Uncomment to disable joystick display
@@ -11,8 +10,8 @@
 #include "../lib/i2c.h"
 task main()
 {
-	 //Setup PrototypeBoard for output on B0 of NXT Port 4 (0x01 represents the pin B0 and Port 4 is defined above in pragma config)
-   HTSPBsetupIO(HTSPB, 0x1);
+	//Setup PrototypeBoard for output on B0 of NXT Port 4 (0x01 represents the pin B0 and Port 4 is defined above in pragma config)
+	HTSPBsetupIO(HTSPB, 0x1);
 	while (true){
 		//port of specifc device
 		int port = 1;
@@ -21,14 +20,14 @@ task main()
 		//Specific to sensor, located in sensor manuals
 		string expected = "ServoCon";
 		bool noerror = errorcheck(port,address,expected);
-	if (noerror){
-		 //LED, we are good
-		HTSPBwriteIO(HTSPB, 0x01);
-	}
-	else {
-   //No LED, because we have error
- 		HTSPBwriteIO(HTSPB, 0x00);
-	}
-	 wait1Msec(1000);
+		if (noerror){
+			//LED, we are good
+			HTSPBwriteIO(HTSPB, 0x01);
+		}
+		else {
+			//No LED, because we have error
+			HTSPBwriteIO(HTSPB, 0x00);
+		}
+		wait1Msec(1000);
 	}
 }
