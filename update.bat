e8032bc7eab6cb21060286f2e3def24eae954246
Arthur Pachachura <pachachura.arthur@gmail.com>
2014-07-04 15:08:03 -0500

    Fix Fixed Version Checking

--- update.bat
+++ update.bat
@@ -1,6 +1,7 @@
 @echo -------------------------------
 @echo Updating version information...
 @del .\robotc\lib\version.ric
+@git rev-parse HEAD >> version.ric
 @git rev-list HEAD | wc -l >> version.ric
 @MOVE version.ric .\robotc\lib\version.ric
 @echo Done!
