776a1745a9ded9b7966e215e6f766b9ca28a4851
Arthur Pachachura <pachachura.arthur@gmail.com>
2014-07-11 11:26:47 -0500

    Copy Updater from NXTLib

--- update.bat
+++ update.bat
@@ -1,9 +1,13 @@
 @echo -------------------------------
 @echo Updating version information...
 @del .\robotc\lib\version.ric
+@del .\tools\update\version.ric
 @git rev-parse HEAD >> version.ric
 @git rev-list HEAD | wc -l >> version.ric
+@COPY version.ric .\tools\update\version.ric
 @MOVE version.ric .\robotc\lib\version.ric
+@cd .\tools\update
+@start UpdateGUI update
 @echo Done!
 @echo -------------------------------
 @pause
