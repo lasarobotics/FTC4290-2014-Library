b67daa2d21d470552be9697baf6c7e233435a3d7
Arthur Pachachura <pachachura.arthur@gmail.com>
2014-07-13 16:46:00 -0500

    Undo That

--- update.bat
+++ update.bat
@@ -3,7 +3,7 @@
 @echo Updating version information...
 @echo -------------------------------
 @del .\tools\update\version.ric
-@git ls-remote git://github.com/lasarobotics/ftclibs.git | grep HEAD | cut -f 1 >> version.ric
+@git ls-remote git://github.com/lasarobotics/ftclibs.git | grep refs/heads/master | cut -f 1 >> version.ric
 @git rev-list HEAD | wc -l >> version.ric
 @MOVE version.ric .\tools\update\version.ric
 @cd .\tools\update
