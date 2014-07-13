71d2e528772853b0cb074413caebff7692629d7c
Arthur Pachachura <pachachura.arthur@gmail.com>
2014-07-13 16:35:29 -0500

    Universalize Origin Checker Reads Parent Successfully Now

--- update.bat
+++ update.bat
@@ -3,7 +3,8 @@
 @echo Updating version information...
 @echo -------------------------------
 @del .\tools\update\version.ric
-@git ls-remote git://github.com/lasarobotics/ftclibs.git | grep refs/heads/master | cut -f 1 >> version.ric
+::ORIGIN SHOULD BE in git:// FORM
+@git ls-remote origin | grep refs/heads/master | cut -f 1 >> version.ric
 @git rev-list HEAD | wc -l >> version.ric
 @MOVE version.ric .\tools\update\version.ric
 @cd .\tools\update
