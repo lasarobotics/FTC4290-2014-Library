c086f67efc43f1f11bfc714b8295f80242ef0ce9
Arthur Pachachura <pachachura.arthur@gmail.com>
2014-07-13 16:37:33 -0500

    Undo That

--- update.bat
+++ update.bat
@@ -3,8 +3,7 @@
 @echo Updating version information...
 @echo -------------------------------
 @del .\tools\update\version.ric
-::ORIGIN SHOULD BE in git:// FORM
-@git ls-remote origin | grep refs/heads/master | cut -f 1 >> version.ric
+@git ls-remote git://github.com/lasarobotics/ftclibs.git | grep refs/heads/master | cut -f 1 >> version.ric
 @git rev-list HEAD | wc -l >> version.ric
 @MOVE version.ric .\tools\update\version.ric
 @cd .\tools\update
