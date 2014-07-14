3a2c0e2bdce08e156e07dfc78ab6de40aaeb8636
Arthur Pachachura <pachachura.arthur@gmail.com>
2014-07-13 21:35:26 -0500

    Fix Your Batch

--- update.bat
+++ update.bat
@@ -4,9 +4,10 @@
 @echo -------------------------------
 @del .\tools\update\version.ric
 @git ls-remote git://github.com/lasarobotics/ftclibs.git | grep refs/heads/master | cut -f 1 >> version.ric
-@git rev-list HEAD | wc -l >> version.ric
+@git rev-list HEAD | wc -l | sed 's/^ *//' >> version.ric
 @MOVE version.ric .\tools\update\version.ric
 @cd .\tools\update
 @start UpdateGUI update
 @echo Done!
 @echo -------------------------------
+@pause
