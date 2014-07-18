8e5dc97d9b320bc8aaa046901c9436d4ac0b37e1
Ehsan Asdar <ehsanmasdar@gmail.com>
2014-07-18 14:42:16 -0500

    Update Mecabot.c

--- robotc/src/Mecabot.c
+++ robotc/src/Mecabot.c
@@ -76,9 +76,9 @@ task main()
         getJoystickSettings(joystick); //get all joystick statuses
 
         mecanum_arcade(deadband(k_deadband,joystick.joy1_y1), 
-        deadband(k_deadband,joystick.joy1_x1), 
-        deadband(k_deadband,joystick.joy1_x2),
-        leftFront, rightFront, leftBack, rightBack);
+		       deadband(k_deadband,joystick.joy1_x1), 
+        	       deadband(k_deadband,joystick.joy1_x2),
+        	       leftFront, rightFront, leftBack, rightBack);
 
         motor[Lf] = leftFront;
         motor[Rf] = rightFront;
