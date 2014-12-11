e5fb608c5225f96623179cd70ac664b17218d439
Oksisane <ehsanmasdar@gmail.com>
2014-12-11 17:52:38 -0600

    Rewrite Gyro Task

--- robotc/lib/gyro.h
+++ robotc/lib/gyro.h
@@ -10,20 +10,18 @@ Implements the HiTechnic Gyroscope.
 #include "../drivers/hitechnic-gyro.h" //gyroscope
 
 /***** LOCAL VARIABLES *****/
-static float rotSpeed = 0;
+float vel_curr = 0.0;
+float vel_prev = 0.0;
 static float heading = 0;
-static float firstrot = 0;
 static tSensors gyro = 0;
 static bool go = true;
 
 /***** QUICK METHODS *****/
 
 //Get current rotational speed (in deg/s)
-float gyro_getrotspeed() { return rotSpeed; }
-//Get current heading from -360 to 360 degrees
+float gyro_getrotspeed() { return vel_curr; }
+//Get current heading
 float gyro_getheading() { return heading; }
-//Get first derivative offset (in deg)
-float gyro_getfirstoffset() { return firstrot; }
 //Reset gyro heading to zero
 void gyro_reset() { heading = 0; }
 //Stop gyro calibration task
@@ -48,36 +46,17 @@ float gyro_init(tSensors link)
 */
 task gyro_calibrate()
 {
+    //Clear Timer
+    time1[T1]=0;
+	float dt = 0.0;
     while (go)
     {
-        // Wait until 20ms has passed
-        while (time1[T1] < 20)
-          wait1Msec(1);
-
-        // Reset the timer
+        vel_prev = vel_curr;
+        dt = (float)time1[T1]/(float)1000.0;
         time1[T1]=0;
-
         // Read the current rotation speed
-        rotSpeed = HTGYROreadRot(gyro);
-        if ((rotSpeed != 0) && (firstrot == 0) && (abs(rotSpeed) < 1))
-        {
-            if (sgn(rotSpeed) == 1) {firstrot = rotSpeed;}
-            if (sgn(rotSpeed) == -1) {firstrot = rotSpeed + 1;}
-        }
-
-        if (sgn(rotSpeed) == 1) {rotSpeed = rotSpeed - firstrot;}
-        if (sgn(rotSpeed) == -1) {rotSpeed = rotSpeed - firstrot + 1;}
-
-        if ((abs(rotSpeed) <= 1) && (firstrot != 0)) { rotSpeed = 0; }
-        if (abs(rotSpeed) < 1) { rotSpeed = 0; }
-
-        // Calculate the new heading by adding the amount of degrees
-        // we've turned in the last 20ms
-        // If our current rate of rotation is 100 degrees/second,
-        // then we will have turned 100 * (20/1000) = 2 degrees since
-        // the last time we measured.
-        heading += rotSpeed * 0.02;
-        if (heading > 360) { heading -= 360; }
-        if (heading < -360) { heading += 360; }
+        vel_curr = (float)HTGYROreadRot(gyro);
+        // Average of last two velocities multiplied by the time passed = change in heading
+        heading += ((float)vel_prev+(float)vel_curr)*(float)0.5*(float)dt;
     }
 }
